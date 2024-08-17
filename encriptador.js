354662b7a540595b05ec3cfca27aade1f98a3ee3
RubyAguiBece <128565531+RubyAguiBece@users.noreply.github.com>
2024-08-17 12:45:29 -0500

    Creando y subiendo los archivos del encriptador
    * Se suben las imágenes que se utilizan.
    * Se implementa en index.
    * Se implementa el css.

---
+++ encriptador.js
@@ -0,0 +1,48 @@
+
+function encriptar() {
+    const textoInput = document.getElementById('text-input').value.trim();
+
+    // Verificar si el texto no está vacío y contiene caracteres no permitidos
+    if (textoInput && !/^[a-z\s]+$/.test(textoInput)) {
+        alert('Por favor, ingrese solo letras minúsculas y espacios.');
+        return;
+    }
+
+    const textoEncriptado = textoInput
+        .replace(/a/g, 'ai')
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+        
+    mostrarResultado(textoEncriptado, 'Texto Encriptado');
+}
+
+function desencriptar() {
+    const textoInput = document.getElementById('text-input').value.toLowerCase();
+    const textoDesencriptado = textoInput
+        .replace(/ai/g, 'a')
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+        
+    mostrarResultado(textoDesencriptado, 'Texto Desencriptado');
+}
+
+function mostrarResultado(texto, titulo) {
+    document.getElementById('text-output-title').innerText = texto ? titulo : 'Lo sentimos';
+    document.getElementById('text-output-text').innerText = texto || 'Ningún mensaje fue encontrado';
+    document.getElementById('text-output-text').classList.toggle('text-encriptado', titulo === 'Texto Encriptado');
+    document.getElementById('text-output-text').classList.toggle('text-desencriptado', titulo === 'Texto Desencriptado');
+    document.getElementById('text-output-image').classList.toggle('hidden', texto.length > 0);
+}
+
+function copiar() {
+    const textoOutput = document.getElementById('text-output-text').innerText;
+    navigator.clipboard.writeText(textoOutput).then(() => {
+        alert('Texto copiado');
+    }).catch(err => {
+        console.error('Error al copiar el texto: ', err);
+    });
+}
