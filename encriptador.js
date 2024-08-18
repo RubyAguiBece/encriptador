30228a0f05afed6aaec5c86362e5bea622e49d0e
RubyAguiBece <128565531+RubyAguiBece@users.noreply.github.com>
2024-08-18 13:08:34 -0500

    Refactorizaccion
    Se corrige la función desencriptar, ya que al cambiar el orden se había dañado el desencriptar

--- encriptador.js
+++ encriptador.js
@@ -9,9 +9,9 @@ function encriptar() {
     }
 
     const textoEncriptado = textoInput
-        .replace(/a/g, 'ai')
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
         
@@ -21,9 +21,9 @@ function encriptar() {
 function desencriptar() {
     const textoInput = document.getElementById('text-input').value.toLowerCase();
     const textoDesencriptado = textoInput
-        .replace(/ai/g, 'a')
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
         
